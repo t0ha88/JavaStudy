ae8bc521b9c61ff64fc2739b86ce56ed758fb760
Anton Kulish <kulag88@gmail.com>
2017-01-12 22:18:27 +0200

    Block #1

---
+++ src/com/javastudy/block1/Human.java
@@ -0,0 +1,22 @@
+package com.javastudy.block1;
+
+public class Human {
+
+    String name;
+    String surname;
+    int age;
+    String address;
+
+    public void say(){
+        System.out.println("Hello, my name is " + name);
+    }
+
+    public void sleep(){
+        System.out.println("I'm sleeping now...");
+    }
+
+    public void eat(){
+        System.out.println("I'm eating...");
+    }
+
+}
---
+++ src/com/javastudy/block1/Main.java
@@ -0,0 +1,12 @@
+package com.javastudy.block1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Human human = new Human();
+
+        human.name = "Anton";
+        human.say();
+    }
+}
