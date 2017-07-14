5d504783b85b0b4eae1eb5feecd9c8c69a209628
anton <kulag88@gmail.com>
2017-07-14 21:54:36 +0300

    Block #7

--- src/com/javastudy/block7/Main.java
+++ src/com/javastudy/block7/Main.java
@@ -5,58 +5,12 @@ import java.lang.reflect.Array;
 public class Main {
     public static void main(String[] args) {
 
+        // TASK #1
         String[] product = {"хлеб", "молоко", "сахар", "мука", "овсянка", "йогурт", "шоколад", "печенье", "бананы", "чай"};
         double[] price = {6, 15, 22.23, 30.09, 20, 5.95, 22, 7.50, 27, 30};
 
-        buildList(product, price);
-
-
-
-
-    }
-
-    public static double getAverSumm (double[] d) {
-        double summ = 0.0;
-
-        for (int i = 0; i < d.length; i++) {
-            summ += d[i];
-        }
-
-        summ = summ / d.length;
-
-        return summ;
-    }
-
-    public static void buildList (String[] pro, double[] pri) {
-        int number = 1;
-        for (int i = 0; i < pro.length; i++) {
-
-            StringBuilder builder = new StringBuilder();
-
-            if (number < 10) {
-                int lengtString = pro[i].length();
-                int space = 10 - lengtString;
-                    for (int j = 0; j < space; j++) {
-                        builder.append(" ");
-                    }
-                String spaceN = builder.toString();
-
-                System.out.println(number + "   |   " + pro[i]  + spaceN + "|   " + pri[i]);
-            } else {
-                int lengtString = pro[i].length();
-                int space = 10 - lengtString;
-                    for (int j = 0; j < space; j++) {
-                        builder.append(" ");
-                    }
-                String spaceN = builder.toString();
-                System.out.println(number + "  |   " + pro[i] + spaceN + "|   " + pri[i]);
-            }
-
-            number++;
-        }
-        System.out.println();
-        System.out.println("Средняя цена товаров = " + getAverSumm(pri));
-
+        Task1 t1 = new Task1();
+        t1.buildList(product, price);
 
     }
 
---
+++ src/com/javastudy/block7/Task1.java
@@ -0,0 +1,48 @@
+package com.javastudy.block7;
+
+public class Task1 {
+
+    public double getAverSumm (double[] d) {
+        double summ = 0.0;
+
+        for (int i = 0; i < d.length; i++) {
+            summ += d[i];
+        }
+
+        summ = summ / d.length;
+
+        return summ;
+    }
+
+    public void buildList (String[] pro, double[] pri) {
+        int number = 1;
+        for (int i = 0; i < pro.length; i++) {
+
+            StringBuilder builder = new StringBuilder();
+
+            if (number < 10) {
+                int lengtString = pro[i].length();
+                int space = 10 - lengtString;
+                for (int j = 0; j < space; j++) {
+                    builder.append(" ");
+                }
+                String spaceN = builder.toString();
+
+                System.out.println(number + "   |   " + pro[i]  + spaceN + "|   " + pri[i]);
+            } else {
+                int lengtString = pro[i].length();
+                int space = 10 - lengtString;
+                for (int j = 0; j < space; j++) {
+                    builder.append(" ");
+                }
+                String spaceN = builder.toString();
+                System.out.println(number + "  |   " + pro[i] + spaceN + "|   " + pri[i]);
+            }
+
+            number++;
+        }
+        System.out.println();
+        System.out.println("Средняя цена товаров = " + getAverSumm(pri));
+
+    }
+}
